e4bf04a4bfc546d242abd3632791256e41457cba
Glenn Trigg <ggtrigg@gmail.com>
2020-05-24 20:08:03 +1000

    Usability tweaks.

--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -2,8 +2,8 @@
 
 class Questionnaire < ApplicationRecord
   belongs_to :user
-  has_many :questions, dependent: :destroy
   has_many :responses, dependent: :destroy
+  has_many :questions, dependent: :destroy
   has_rich_text :description
 
   accepts_nested_attributes_for :questions, allow_destroy: true
