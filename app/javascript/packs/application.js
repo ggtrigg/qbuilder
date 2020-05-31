211923a5986b8f37cbda66b4349b7cd4279de010
Glenn Trigg <ggtrigg@gmail.com>
2020-05-31 12:09:51 +1000

    Only support jpg & png uploads.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,7 @@ require("jquery")
 // require("popper")
 // require("bootstrap")
 import 'bootstrap'
+import '../trix-editor-overrides'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
