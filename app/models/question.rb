8455ee7f694e1dddade38cc5cad83a99e6aa8632
Glenn Trigg <ggtrigg@gmail.com>
2020-06-01 09:00:02 +1000

    Refactoring choices, UI tweaks, inline choices.

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,4 +4,15 @@ class Question < ApplicationRecord
   enum answer_type: [:true_false, :yes_no, :single_line, :multi_line, :multiple_choice_single, :multiple_choice_any]
 
   validates :blurb, presence: true
+
+  def choices
+    read_attribute(:choices).split /\R/ if read_attribute(:choices).present?
+  end
+
+  def inlineable?
+    max_words = choices.map {|c| c.split.count }.max
+    average_length = choices.map {|c| c.length }.sum / choices.count
+
+    (max_words < 4) && (average_length < 30)
+  end
 end
