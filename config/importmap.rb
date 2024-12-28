e45c0a086d4d5e17710121bf7b025ff43809e137
Glenn Trigg <ggtrigg@gmail.com>
2024-12-28 15:43:02 +1100

    Remove jquery from importmap.

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,7 +6,6 @@ pin "@rails/activestorage", to: "activestorage.esm.js"
 pin "@rails/actiontext", to: "actiontext.esm.js"
 pin "trix" # @2.1.8
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "jquery" # @3.7.1
 pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.esm.min.js", preload: true # @5.3.3
 pin "@popperjs/core", to: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/esm/popper.min.js" # @2.11.8
 pin "sass" # @1.81.0
