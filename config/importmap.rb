67a7d2bf8c206cb293e26c45f777bfc159a0c1f3
Glenn Trigg <ggtrigg@gmail.com>
2026-01-01 10:34:18 +1100

    Package updates & use gem supplied trix.js

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,9 +4,10 @@ pin "application"
 pin "trix-editor-overrides"
 pin "@rails/actioncable", to: "actioncable.esm.js"
 pin "@rails/activestorage", to: "activestorage.esm.js"
+pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "sass" # @1.94.2
+pin "sass" # @1.97.1
 pin "buffer" # @6.0.3
 pin "immutable" # @5.1.4
 pin "process" # @2.1.0
@@ -14,10 +15,9 @@ pin "fontawesome" # @5.6.3
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@stimulus-components/sortable", to: "@stimulus-components--sortable.js" # @5.0.3
-pin "@rails/request.js", to: "@rails--request.js.js" # @0.0.12
+pin "@rails/request.js", to: "@rails--request.js.js" # @0.0.13
 pin "sortablejs" # @1.15.6
 pin "nouislider" # @15.8.1
-pin "trix" # @2.1.15
 pin "base64-js" # @1.5.1
 pin "ieee754" # @1.2.1
 pin "bootstrap", preload: true
